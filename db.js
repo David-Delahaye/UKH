89cea819cae4cd06c7596dc2a6ff326d62d04306
David Delahaye <delahaye3@hotmail.co.uk>
2020-06-22 14:43:13 +0100

    add env port

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const {Pool} = require ('pg');
 require('dotenv/config');
-console.log(process.env.LOCAL_STRING);
 
 const pool = new Pool({
     connectionString: (process.env.DATABASE_URL || process.env.LOCAL_STRING)
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const pool = require('./db.js');
+const PORT = (process.env.PORT || 5000);
 require('dotenv/config');
 app.use(express.json())
 
@@ -61,7 +62,7 @@ app.delete('/sites/:site', async (req,res) => {
 })
 
 
-app.listen(5000, function(){
-    console.log('listening at 5000');
+app.listen(PORT, function(){
+    console.log('listening at ' + PORT);
     
 });
