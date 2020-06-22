a871c6cf0d7214df8dba838637e298ae8422e551
David Delahaye <delahaye3@hotmail.co.uk>
2020-06-22 21:57:45 +0100

    adding build to source control

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const PORT = (process.env.PORT || 5000);
 const cors = require('cors');
 
 //MiddleWare
-app.use(express.static(path.join(__dirname, 'client/public')))
+app.use(express.static(path.join(__dirname, 'client/build')))
 app.use(cors());
 app.use(express.json())
 
 // INDEX
 app.get('/', async (req,res) => {
-    console.log(path.join(__dirname, 'client/public', 'index.html'));
-    res.sendFile(path.join(__dirname, 'client/public', 'index.html'))
+    console.log(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 app.get('/api/sites', async (req,res) => {
