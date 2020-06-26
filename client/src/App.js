ed6505f0cddd6acc71cb44cc256948a1aaf7182c
David Delahaye <delahaye3@hotmail.co.uk>
2020-06-26 04:19:28 +0100

    add delete site route front

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Redirect } from 'react-router-dom';
 
 class Sites extends Component {
   constructor(){
     super();
     this.state = {
+      redirect:false,
       sites:[]
     }
   }
@@ -29,12 +31,31 @@ class Sites extends Component {
     }
   }
 
+  deleteSite = async (e,i) => {
+    const newSites = this.state.sites.slice();
+    newSites.splice(i,1);
+    console.log(newSites);
+    this.setState({sites:newSites});
+    console.log(this.state.sites);  
+    try {
+      const id = e.site_id;
+      console.log(id);
+      const response = await fetch(`/api/sites/${id}`,{
+        method: "DELETE",
+        headers: { "Content-Type" : "application/json"},
+      });
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
   render(){
     let siteFormat = this.state.sites.map ((e,i) => {
       return(
         <div key={i}>
         <h3>{e.site_name}</h3>
         <p>{e.description}</p>
+        <button onClick = {() => this.deleteSite(e,i)}>Delete</button>
         </div>
       )
     })
@@ -52,6 +73,7 @@ class NewSite extends Component {
   constructor(){
     super();
     this.state = {
+      redirect:false,
       inputs:{
         title:'',
         description:''
@@ -66,17 +88,21 @@ class NewSite extends Component {
         const desc = e.target.desc.value;
         const body = {"siteName":title, "siteDesc":desc}
         const response = await fetch("/api/sites",{
-        method: "POST",
-        headers: { "Content-Type" : "application/json"},
-        body: JSON.stringify(body)
-      });
+          method: "POST",
+          headers: { "Content-Type" : "application/json"},
+          body: JSON.stringify(body)
+        });
+        this.setState({redirect:true})
     } catch (err) {
       console.error(err.message);
-      
     }
+   
   }
 
   render(){
+    if (this.state.redirect === true){
+      return<Redirect to ="/index"/>
+    }
     return(
       <form onSubmit = {(e) =>{this.formSubmit(e)}}>
         <input type='text' name ='title' placeholder='add a site'/>
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import App from './App.js';
 const {Sites, NewSite} = App;
 
 ReactDOM.render(
   <React.StrictMode>
-    <Sites />
-    <NewSite/>
+    <Router>
+      <Switch>
+        <Route path="/new">
+          <NewSite />
+        </Route>
+        <Route path="/index">
+          <Sites />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
