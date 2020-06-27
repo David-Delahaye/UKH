8def9fbd5f5246f2a40a650e3b27cbf4f76a2564
David Delahaye <delahaye3@hotmail.co.uk>
2020-06-27 21:54:04 +0100

    define nav

--- newtable.sql
+++ newtable.sql
@@ -1,5 +1,5 @@
 CREATE TABLE site(
-    site_id serial PRIMARY KEY,
+    site_id uuid DEFAULT uuid_generate_v4(),
     site_name VARCHAR (50) NOT NULL,
     description VARCHAR (1000) NOT NULL,
     created_on TIMESTAMP NOT NULL
