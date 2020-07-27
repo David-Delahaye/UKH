9b4ac5bea8255f33b9ab6471267d5bf3f5a7e930
David Delahaye <delahaye3@hotmail.co.uk>
2020-07-27 22:41:58 +0100

    add timeout to messages

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -9,10 +9,31 @@ import form from "../modules/form/form.module.css";
 class Messages extends Component {
     constructor(props) {
       super(props);
+      this.state = {
+        currentMessage:'',
+        messageActive:false
+      }
     }
+
+
     render(){
+
+      //on message change
+        if (this.props.message.content !== this.state.currentMessage){
+        console.log('newMessage');
+        this.setState({currentMessage:this.props.message.content})
+        this.setState({messageActive:true})
+        setTimeout(() => {this.setState({messageActive:false})}, 3000);
+        }
+      
+      //display message
+        if (this.state.messageActive){
         return(
-        <div>{this.props.message.content}</div>
+        <div className='message'>{this.props.message.content}</div>
+        )
+        }
+        return(
+          ''
         )
     }
 }
